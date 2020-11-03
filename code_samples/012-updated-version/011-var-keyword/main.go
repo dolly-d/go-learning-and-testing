597c8437269e92341c923a5d6de0d089af70732c
dolly-d <dolly.desir@gmail.com>
2020-11-02 19:49:11 -0500

    going

--- code_samples/012-updated-version/011-var-keyword/main.go
+++ code_samples/012-updated-version/011-var-keyword/main.go
@@ -4,31 +4,22 @@ import (
 	"fmt"
 )
 
-// DECLARE the variable "y"
-// ASSIGN the value 43
-// declare & assign = initilization
-var d = 35
-
-// DECLARE there is a VARIABLE with the IDENTIFIER "z"
-// and that the VARIABLE with the IDENTIFIER "z" is of TYPE int
-// ASSIGNS the ZERO VALUE of TYPE int to "z"
-// false for booleans, 0 for integers, 0.0 for floats, "" for strings,
-// and nil for pointers, functions, interfaces, slices, channels, and maps.
-var z int
+type chicken int
+var x chicken
+var y int 
 
 func main() {
-	// short declaration operator
-	// DECLARE a variable and ASSIGN a VALUE (of a certain TYPE)
-	x := 42
+	
 	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 3
+	fmt.Println(x)
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T", y)
 
-	fmt.Println(d)
-
-	foo()
-
-	fmt.Println(z)
 }
 
-func foo() {
-	fmt.Println("again:", d)
-}
+
+
+
