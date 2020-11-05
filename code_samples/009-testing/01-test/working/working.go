8f90d135cc5a44a6719ce0d1c2df26263ab4776d
dolly-d <dolly.desir@gmail.com>
2020-11-05 10:23:17 -0500

    still looping

--- code_samples/009-testing/01-test/working/working.go
+++ code_samples/009-testing/01-test/working/working.go
@@ -11,5 +11,21 @@ func main () {
 		 j < 3; j++ {
 			 fmt.Printf("The outer loop: %d\t The inner loop: %d\n", i, j)
 		 }
+
+	}
+	divide () 
+	testing ()
+
+}
+
+func divide () {
+	x := 83 / 40
+	y := 83 % 40 
+	fmt.Println(x, y)
+}
+
+func testing () {
+	for i := 33; i < 122; i++{
+		fmt.Printf("%v\t%#Un", i, i)
 	}
 }
